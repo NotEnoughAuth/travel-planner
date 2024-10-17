bda655ae4b2750c7ecbdb467f5617e1729e095a0
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2024-10-17 15:49:53 -0500

    Working version of weather API call
    Initial working version, next step is to finish fancifying the result formatting

--- weather-api.py
+++ weather-api.py
@@ -2,20 +2,27 @@ import requests
 import os
 import json
 
+# Function to get the current weather based on City
 def get_weather(city):
+    # Load the API key
     with open('apikeys.json') as f:
         data = json.load(f)
         api = data['openweatherAPI_key']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}'
+
+    # Send a request to the OpenWeatherMap API
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=imperial'
     response = requests.get(url)
     data = response.json()
+
+    # Parse the data for wanted information
     parsed_data = {
-        'city': data['name'],
+        'city': city,
+        #'weather': data['weather'][0]['main'],
         'temperature': data['main']['temp'],
-        'description': data['weather'][0]['description'],
-        'icon': data['weather'][0]['icon']
+        'description': data['weather'][0]['description']
     }
-    return parsed_data
 
+    # Format the wanted information to human readable format
+    result = f"Current weather in {parsed_data['city']} is {parsed_data['description']} with a temperature of {parsed_data['temperature']}F."
 
-get_weather('London')
+    return result
