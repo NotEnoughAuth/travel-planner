bcca03e9b1be23ae37753f974f0b2a2160a61329
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2024-10-17 15:35:14 -0500

    Updated .gitignore and openweather API calls

--- weather-api.py
+++ weather-api.py
@@ -1,7 +1,12 @@
 import requests
+import os
+import json
 
 def get_weather(city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY'
+    with open('apikeys.json') as f:
+        data = json.load(f)
+        api = data['openweatherAPI_key']
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}'
     response = requests.get(url)
     data = response.json()
     parsed_data = {
@@ -11,3 +16,6 @@ def get_weather(city):
         'icon': data['weather'][0]['icon']
     }
     return parsed_data
+
+
+get_weather('London')
