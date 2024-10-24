4e72e8fa577677c51d6a34d2a6054a803bb9b1d3
Owen Pryga <opryga@oplabs.us>
2024-10-24 14:02:49 -0500

    Fixed hotelAPI

--- module1.py
+++ module1.py
@@ -1,35 +1,41 @@
 import requests # type: ignore
 import json
 
-with open('apikeys.json') as f:
+
+def Hotel_Api(lat, long, check_in_date, check_out_date, guests):
+    with open('apikeys.json') as f:
         api_data = json.load(f)
-        api_key = api_data['{dbc186abbdmsh821a1681fa1333ap152100jsnf9c1d76adf45}_api_key']
+        api_key = api_data['tripAdvisorAPI']
 
-
-def Hotel_Api(location, check_in_date, check_out_date, guests):
     # API base URL for Amadeus Hotel Search
-    base_url = "tripadvisor16.p.rapidapi.com"
-    
+    base_url = "https://tripadvisor16.p.rapidapi.com//api/v1/hotels/searchHotelsByLocation"
     
  
     
     # Parameters to pass in the API request
     params = {
-        "cityCode": location,  # Use IATA city code
-        "checkInDate": check_in_date,
-        "checkOutDate": check_out_date,
+        "latitude": lat, # latitude and longitude of the location
+        "longitude": long,
+        "checkIn": check_in_date,
+        "checkOut": check_out_date,
         "adults": guests,
-        "apikey": api_key
     }
+
+    headers = {
+        "x-rapidapi-key": api_key,
+    }
+
+    print(params)
     
     try:
         # Send the request to the API
-        response = requests.get(base_url, params=params)
+        response = requests.get(base_url, params=params, headers=headers)
         response.raise_for_status()  # Raise an exception for HTTP errors
         
         # Parse and return the hotel data
         hotel_data = response.json()
-        return hotel_data.get('data', [])  # Assuming the API response contains a 'data' key with the results
+        print(hotel_data["data"]["data"])
+        return hotel_data["data"]["data"]  # Assuming the API response contains a 'data' key with the results
     except requests.exceptions.RequestException as e:
         # Handle errors or failures
         print(f"Error: {e}")
@@ -37,7 +43,7 @@ def Hotel_Api(location, check_in_date, check_out_date, guests):
 
 if __name__ == "__main__":
     # Example usage:
-    hotels = Hotel_Api("NYC", "2024-11-01", "2024-11-05", 2)
+    hotels = Hotel_Api("33.954737", "-118.212016", "2024-11-01", "2024-11-05", 2)
     if hotels:
         for hotel in hotels:
-            print(f"Hotel: {hotel['name']}, Price: {hotel['price']}")
+            print(f"Hotel: {hotel['title']}, Price: {hotel['priceForDisplay']}")
